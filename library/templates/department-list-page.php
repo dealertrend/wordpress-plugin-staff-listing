7ebe1b8a47d78e060130a27b9268f1084c580026
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-01-31 11:22:05 -0800

    Adding rows markup to the department list page.

--- library/templates/department-list-page.php
+++ library/templates/department-list-page.php
@@ -4,6 +4,7 @@
   global $post;
 
   # TODO: Add pagination...right now I think this is limited to 10 entries...by default.
+  # TODO: Fix sorting!
   $parameters = array(
     'orderby' => 'name',
     'order' => 'ASC',
@@ -15,17 +16,29 @@
 
   $departments = array_filter( explode( '<br />' , wp_list_categories( $parameters ) ), 'trim' );
 
+  $limit = count( $departments );
+
+  $counter = 0;
+  $rows = 0;
+
   get_header();
 ?>
   <div id="staff-listing" class="department-listing">
     <div class="wrapper">
       <h3 class="header">Departments</h3>
       <?php if( $departments[ 0 ] != 'No categories' ) : foreach( $departments as $department ) : ?>
+      <?php if( $counter % 4 == 0 || $counter == 0 ): $rows++; ?>
+        <div class="row">
+      <?php endif; $counter++; ?>
         <div class="list-item">
           <h2 class="name">
             <?php echo trim( $department ); ?>
           </h2>
         </div>
+        <?php if( $counter % 4 == 0 || $counter == $limit ):  ?>
+          <div style="clear:both;"></div>
+          </div><!-- .row -->
+        <?php endif; ?>
       <?php endforeach; else: ?>
         <p>Sorry, there are currently no active departments.</p>
       <?php endif; ?>
