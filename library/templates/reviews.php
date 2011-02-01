2189acfa386c59cc44bedc2a3f31b9813ec7dce3
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-02-01 11:48:58 -0800

    Fixing mark-up, also fixing logic being too greedy with the mark-up.

--- library/templates/reviews.php
+++ library/templates/reviews.php
@@ -33,24 +33,24 @@
   <?php if( $current_user->ID ): ?>
     <h3>Add Reviews</h3>
     <form action="<?php echo get_option( 'siteurl' ); ?>/wp-comments-post.php" method="post" id="commentform">
-      <p>
+      <p> 
         Logged in as <a href="<?php echo get_option( 'siteurl' ); ?>/wp-admin/profile.php"><?php echo $current_user->user_nicename; ?></a>. | 
         <a href="<?php echo wp_logout_url( get_permalink() ); ?>" title="Log out of this account">Log out &raquo;</a>
       </p>
       <p><textarea name="comment" id="comment" cols="100" rows="10" tabindex="4"></textarea></p>
-      <p>
+      <p> 
         <input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
         <?php comment_id_fields(); ?>&nbsp;<?php cancel_comment_reply_link( "Cancel reply" ); ?>
       </p>
       <?php do_action( 'comment_form' , $post->ID ); ?>
     </form>
   <?php endif; # Do we know who the user is? ?>
-</div><!-- #review -->
 
 <?php endif; # Is the staff member open for review? ?>
 
-</div><!-- #staff-listing-comments -->
-
-</div><!-- This is a saftey net. It's needed because of the exit() -->
+</div><!-- #review -->
+</div><!-- #reviews -->
+</div><!-- .postcomments  -->
+</div><!-- #single -->
 
 <?php get_footer(); ?>
